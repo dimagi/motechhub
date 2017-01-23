48ad0de314293f349897a29396080ae548640469
Daniel Roberts <droberts@dimagi.com>
2017-01-23 15:34:57 -0500

    add django admin

--- openmrs/admin.py
+++ openmrs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from openmrs.concepts.models import OpenmrsConcept
+from openmrs.credentials.models import OpenmrsInstance, OpenmrsCredential
 
-# Register your models here.
+
+class OpenmrsInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+class OpenmrsCredentialAdmin(admin.ModelAdmin):
+    pass
+
+
+class OpenmrsConceptAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(OpenmrsInstance, OpenmrsInstanceAdmin)
+admin.site.register(OpenmrsCredential, OpenmrsCredentialAdmin)
+admin.site.register(OpenmrsConcept, OpenmrsConceptAdmin)
