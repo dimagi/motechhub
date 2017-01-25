a4d0ae4a835f4c9cd687b0710b55543cdf893029
Daniel Roberts <droberts@dimagi.com>
2017-01-25 16:02:00 -0500

    give OpenmrsConcept a display function

--- openmrs/concepts/models.py
+++ openmrs/concepts/models.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 import json
 from django.db import models
 from openmrs.credentials.models import OpenmrsInstance
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class OpenmrsConcept(models.Model):
     uuid = models.CharField(max_length=256, primary_key=True)
     instance = models.ForeignKey(OpenmrsInstance)
@@ -14,3 +16,6 @@ class OpenmrsConcept(models.Model):
     answers = models.ManyToManyField('OpenmrsConcept')
     descriptions = models.TextField(validators=(json.dumps,))
     names = models.TextField(validators=(json.dumps,))
+
+    def __str__(self):
+        return 'OpenMRS Concept {}: {}'.format(self.uuid, self.display)
