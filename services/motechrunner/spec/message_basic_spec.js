069e6f14a5e621f4b29798fbc9fb82e99b798107
Daniel Roberts <droberts@dimagi.com>
2017-03-17 09:51:44 -0400

    test ordering in list message api

--- services/motechrunner/spec/message_basic_spec.js
+++ services/motechrunner/spec/message_basic_spec.js
@@ -24,6 +24,24 @@ create('Post message')
   })
   .toss();
 
+create('Post second message')
+  .post(URL + '/testmessagestream/message', null, {
+    body: JSON.stringify({
+      type: 'Character',
+      name: 'Popeye',
+      favoriteFood: 'Spinach',
+    })
+  })
+  .expectStatus(201)
+  .expectJSON({
+    ok: true
+  }).expectJSONTypes({
+    id: 'String'
+  }).afterJSON((json) => {
+    messageId = json.id;
+  })
+  .toss();
+
 create('Get messages without limit fails')
   .get(URL + '/testmessagestream/message')
   .expectStatus(400)
@@ -37,15 +55,24 @@ create('Get messages')
   .get(URL + '/testmessagestream/message?limit=10')
   .expectStatus(200)
   .afterJSON((json) => {
-    expect(json.length).toBe(1);
-    json.forEach((message) => {
-        expect(message.created).toMatch(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d\d\d\dZ$/);
-        expect(message.body).toEqual({
-          type: 'Character',
-          name: 'Homer',
-          favoriteFood: 'Donut',
-        });
+    expect(json.length).toBe(2);
+    // they will appear in reverse order from how they were sent
+    let message1 = json[0];
+    let message2 = json[1];
+    expect(message1.created).toMatch(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d\d\d\dZ$/);
+    expect(message1.body).toEqual({
+      type: 'Character',
+      name: 'Popeye',
+      favoriteFood: 'Spinach',
     });
+
+    expect(message2.created).toMatch(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d\d\d\dZ$/);
+    expect(message2.body).toEqual({
+      type: 'Character',
+      name: 'Homer',
+      favoriteFood: 'Donut',
+    });
+
   })
   .toss();
 
