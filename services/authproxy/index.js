84a20459a1beb35550fcb12ec2e862e8273f82c9
Daniel Roberts <droberts@dimagi.com>
2017-03-23 16:19:30 -0500

    create index.js file for authproxy

---
+++ services/authproxy/index.js
@@ -0,0 +1,4 @@
+exports.Crypto = require('./crypto.js').Crypto;
+exports.CredentialDatabase = require('./credential-database.js').CredentialDatabase;
+exports.AuthProxy = require('./authproxy.js').AuthProxy;
+exports.DummyServer = require('./dummyserver.js').DummyServer;
