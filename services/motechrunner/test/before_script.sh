8721f7c273f41794830ad07a3ea18746c9627f3a
Daniel Roberts <droberts@dimagi.com>
2017-03-23 11:26:27 -0500

    travis: migrate db

--- services/motechrunner/test/before_script.sh
+++ services/motechrunner/test/before_script.sh
@@ -2,4 +2,5 @@
 
 psql -c "CREATE USER motechrunner WITH PASSWORD 'motechrunner' CREATEDB;" -U postgres
 createdb -U motechrunner motechrunner
+python manage.py migrate
 python manage.py runserver 7001 &
